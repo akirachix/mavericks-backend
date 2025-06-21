05277433c219879b1b2e134a3343190875f1dded
esther ahenda <eahenda5@gamil.com>
2025-06-21 11:28:08 +0300

    created an app for notifications

---
+++ mitumbaesales/notifications/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Notification
+from authentication.models import User
+
+# Register your models here.
+
+admin.site.register(Notification)
+admin.site.register(User)
