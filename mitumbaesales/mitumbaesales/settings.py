c86dcb603632fbf48c9945c0f63e51363e44b6fe
wangara <mercywangarakwagalakwe@gmail.com>
2025-06-21 15:05:55 +0300

    Created the cart app

--- mitumbaesales/mitumbaesales/settings.py
+++ mitumbaesales/mitumbaesales/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notification',
-    'cart',  
-    'product',  
+    'cart',    
     'order'
     'authentication'
     'product',
