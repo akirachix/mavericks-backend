a02c30ee1b92a68505af43acfa6867c4b7d73230
mahado <mahadoaliyussuf@gmail.com>
2025-06-21 14:06:15 +0300

    Add Notification app with model and admin setup for user alerts

--- mitumbaesales/mitumbaesales/settings.py
+++ mitumbaesales/mitumbaesales/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-feature/offers
-     'offer',
+    'notification',
+ 
 
-    'payments'
- feature/boilerplate
 ]
 
 MIDDLEWARE = [
