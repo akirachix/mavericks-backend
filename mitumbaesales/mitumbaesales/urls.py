15ce180b30a9d58f61ff0744242e680f1b104521
Meaza Misgina <misginameaza@gmail.com>
2025-06-25 17:28:39 +0300

    modified app

--- mitumbaesales/mitumbaesales/urls.py
+++ mitumbaesales/mitumbaesales/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('product.urls')),
+    path('api/', include('authentication.urls')),
+    path('api/', include('payments.urls')),
+    path('api/', include('cart.urls')),
+    path('api/', include('order.urls')),
 ]
+
+
+
