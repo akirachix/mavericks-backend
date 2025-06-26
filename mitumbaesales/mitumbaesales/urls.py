87c839a049bb41b308198ac1bfc7f68fc9fa2d06
Meaza Misgina <misginameaza@gmail.com>
2025-06-26 21:34:29 +0300

    Updated cart/models.py and mitumbaesales/urls.py files

--- mitumbaesales/mitumbaesales/urls.py
+++ mitumbaesales/mitumbaesales/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/offer/', include('offer.urls')),
-    path('api/notification/', include('notification.urls')),
-    path('api-auth/', include('rest_framework.urls')),
 
 ]
