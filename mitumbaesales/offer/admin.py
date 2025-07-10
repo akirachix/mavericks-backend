0a359de3c0f82178b285de003fd0c2a11418cd5f
mahadoali <mahadoaliyussuf@gmail.com>
2025-07-10 10:05:20 +0300

    Update admin.py
    Uncommented offer/admin.py

--- mitumbaesales/offer/admin.py
+++ mitumbaesales/offer/admin.py
@@ -1,13 +1,7 @@
-# from django.contrib import admin
-# from .models import Offer, Discount
-# # OfferDiscount, OfferProduct
-# #from .models import Product
+from django.contrib import admin
+from models import Offer, Discount
+OfferDiscount, OfferProduct
+from models import Product
 
-# # Register your models here.
+|#Register your models here.
 
-
-# admin.site.register(Offer)
-# admin.site.register(Discount)
-# # admin.site.register(OfferDiscount)
-# # admin.site.register(OfferProduct)
-# #admin.site.register(Product)
