6b00c523b2d724b851f429739d6cea7367bd24f4
magretjumbedavid <magretjumbedavid@gmail.com>
2025-07-11 17:31:22 +0300

    fixed conflicts

--- mitumbaesales/mitumbaesales/urls.py
+++ mitumbaesales/mitumbaesales/urls.py
@@ -15,18 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('api/', include('api.urls')),
-=======
-    path('admin/', admin.site.urls),path("api/",include("api.urls")),
->>>>>>> feature/boilerplate
 ]
-=======
 
-]
->>>>>>> 87c839a049bb41b308198ac1bfc7f68fc9fa2d06
